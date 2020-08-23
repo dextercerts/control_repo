88aa2da162401091bd2367ed99f65390820e75c0
dextercerts <69430196+dextercerts@users.noreply.github.com>
2020-08-24 00:34:55 +0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,15 @@
-node default {
-  file {'/root/README':
-    ensure  => file,
-    content => $fqdn,
-    owner   => 'root',
-  }  
+node default {  
   class { 'ntp':
     servers => ['3.ae.pool.ntp.org','0.asia.pool.ntp.org','2.asia.pool.ntp.org']
   }  
 }
 node 'puppetm.dexterslab.local' {
   include role::master_server
+  file {'/root/README':
+    ensure  => file,
+    content => $fqdn,
+    owner   => 'root',
+  }
 }
 
 node /^web/ {
