0cc55227e658da49818279c0ba12e04e6eab117a
dextercerts <69430196+dextercerts@users.noreply.github.com>
2020-08-24 01:48:31 +0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,6 +2,7 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.dexterslab.local':}
   dockeragent::node {'db.dexterslab.local':}
+  dockeragent::node {'minetest.dexterslab.local':}
   host {'web.dexterslab.local':
     ensure => present,
     ip     => '172.18.0.2',
